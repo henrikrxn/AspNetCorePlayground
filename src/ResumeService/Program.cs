f8cb2e9e959edcd9cbfa01b777b44ee3cd4971ab
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2023-12-03 13:14:57 +0100

    Adding Serilog and .NET 8 HTTP request logging

--- src/ResumeService/Program.cs
+++ src/ResumeService/Program.cs
@@ -1,42 +1,126 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
+using Serilog.Events;
+using System.Diagnostics;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithMachineName()
+    .Enrich.WithProcessId()
+    // If ApplicationInsights needs to be added then the configuration will have to be built first
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Properties:j}{NewLine}{Exception}")
+    // If you want to see the context and added properties, use
+    // outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Properties:j}{NewLine}{Exception}"
+    // as argument for Console() above
+    .CreateBootstrapLogger();
 
-var app = builder.Build();
+Log.Information("Starting up");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+    builder.Configuration.AddUserSecrets(typeof(Program).Assembly, optional: true, reloadOnChange: true);
 
-var summaries = new[]
-{
+    // Logging in general
+
+    // Clearing the pre-registered providers so we know exactly what has been setup
+    builder.Logging.ClearProviders();
+
+    // New ASP.NET Core 8 HTTP logging
+    builder.Services.AddHttpLogging(logging =>
+    {
+        logging.LoggingFields = HttpLoggingFields.All;
+        logging.CombineLogs = true;
+    });
+
+    // Serilog internal debug logging
+    if (builder.Environment.IsDevelopment())
+    {
+        Serilog.Debugging.SelfLog.Enable(msg => Debug.WriteLine(msg));
+        Serilog.Debugging.SelfLog.Enable(Console.Error);
+    }
+
+    builder.Host.UseSerilog((context, services, configuration) => {
+
+        configuration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithMachineName()
+            .Enrich.WithProcessId()
+            .Enrich.FromLogContext();
+
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            configuration.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Properties:j}{NewLine}{Exception}");
+            // If you want to see the context and added properties, use
+            // outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Properties:j}{NewLine}{Exception}"
+            // as argument for Console() above
+        }
+        else
+        {
+            // If console is deemed in Azure environments there it is probably a good idea to add
+            // https://nuget.org/packages/serilog.sinks.async
+            // as console historically has been known to slow things down a lot
+            configuration.WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Error); // Console is terribly ineffective, so limiting to the really bad stuff
+        }
+    }, writeToProviders: true);
+
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    // The normal Microsoft request logging
+    app.UseHttpLogging();
+
+    var summaries = new[]
+    {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
 
-app.Run();
+    app.Run();
+
+    Log.Information("Application stopped normally");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled problems during application setup and startup");
+}
+finally
+{
+    Log.Information("Flushing and closing Serilog");
+    Log.CloseAndFlush();
+}
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
