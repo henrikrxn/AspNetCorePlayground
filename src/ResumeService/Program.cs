89bd4f3683d9296da056264278ef20cced08d7a7
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2023-12-04 07:38:38 +0100

    First simple version of HTTP integration tests

--- src/ResumeService/Program.cs
+++ src/ResumeService/Program.cs
@@ -126,3 +126,6 @@ internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+// Expose the Program class so that WebApplicationFactory<T> can access it
+public partial class Program { }
