d926cb2c8bff815aa65dd9210f69f9afc24dddae
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2025-08-14 08:36:35 +0200

    Moving set-up of application endpoints to extension method

--- src/AspNetCorePlayground/Program.cs
+++ src/AspNetCorePlayground/Program.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography;
 using AspNetCorePlayground;
 using AspNetCorePlayground.Plumbing;
 using AspNetCorePlayground.Plumbing.Configuration;
+using AspNetCorePlayground.Plumbing.Endpoints;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
@@ -202,46 +203,7 @@ try {
     //
     // Endpoints
     //
-    _ = app.MapGet("/weatherforecast", static (ILogger<Program> logger) =>
-        {
-            logger.GeneratingWeatherForecast();
-
-            string[] summaries = [ "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" ];
-            WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    (
-                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        RandomNumberGenerator.GetInt32(-20, 55),
-                        summaries[RandomNumberGenerator.GetInt32(0, summaries.Length)]
-                    ))
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast")
-        .WithOpenApi();
-
-    _ = app.MapGet("/throws", static (ILogger<Program> logger) =>
-    {
-        logger.AboutToThrowException();
-
-        throw new WebException("My exception");
-    });
-
-    _ = app.MapGet("/internalServerError", static (ILogger<Program> logger) =>
-    {
-        logger.InternalServerError();
-
-        return TypedResults.InternalServerError("Something went wrong!");
-    });
-
-    _ = app.MapGet("/config/dictionary", (ILogger<Program> logger, IOptions<DictionaryConfiguration> dictionaryOptions) =>
-    {
-        logger.GettingConfiguration();
-
-        DictionaryConfiguration dictionary = dictionaryOptions.Value;
-
-        return dictionary.Items is { Count: > 0 } ? Results.Ok(dictionary.Items) : Results.NotFound("Configuration items not found.");
-    });
+    _ = app.MapApplicationEndpoints();
 
     loggerAfterBuild.StartingApplication();
 
