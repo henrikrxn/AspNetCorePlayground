325b0378f250dc6ef360fc6319369252819fd1ed
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2026-05-09 11:38:07 +0200

    Introduce GeodeticEarthSurfaceDto and refactoring GeodeticEarthSurface
    * Adding tests for required Latitude and Longitude

--- src/AspNetCorePlayground/Domain/GeodeticEarthSurface.cs
+++ src/AspNetCorePlayground/Domain/GeodeticEarthSurface.cs
@@ -4,11 +4,7 @@ namespace AspNetCorePlayground.Domain;
 
 public class GeodeticEarthSurface
 {
-    [Required]
-    [Range(-90.0,90.0)]
     public decimal Latitude { get; init; }
 
-    [Required]
-    [Range(-180.0,180.0)]
     public decimal Longitude { get; init; }
 }
