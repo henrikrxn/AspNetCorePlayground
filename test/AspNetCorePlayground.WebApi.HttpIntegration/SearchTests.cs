b2915ef12cefc0aaa1ca0e764e468fcacc1bfb96
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2026-07-22 10:59:40 +0200

    Search: Adding test case for unsupport media type

--- test/AspNetCorePlayground.WebApi.HttpIntegration/SearchTests.cs
+++ test/AspNetCorePlayground.WebApi.HttpIntegration/SearchTests.cs
@@ -1,3 +1,8 @@
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Xml.Serialization;
+
 namespace AspNetCorePlayground.WebApi.HttpIntegration;
 
 [Collection(ResumeAppCollectionFixture.Name)]
@@ -12,13 +17,6 @@ public sealed class SearchTests
         Fixture = fixture;
         TestOutputHelper = testOutputHelper;
     }
-
-    // Duplicating instead of referencing the ones in the Project in order to test the "contract" not the existing implementation
-#pragma warning disable CA1812
-    internal sealed record SearchRequest(int ProductNumber);
-    internal sealed record SearchResponse(string Sku);
-
-#pragma warning restore CA1812
 
     [Fact]
     public async Task WhenCallingSearch_ThenGetProductNumber()
@@ -37,4 +35,36 @@ public sealed class SearchTests
         SearchResponse searchResponse = readFromJson.ShouldNotBeNull();
         searchResponse.Sku.ShouldBe("Product42");
     }
+
+    [Fact]
+    public async Task WhenCallingSearchWithContentTypeXml_ThenFailsWithUnsupportedMediaType()
+    {
+        // Arrange
+        using HttpClient client = Fixture.CreateClient();
+        var requestData = new SearchRequest(42);
+        var serializer = new XmlSerializer(typeof(SearchRequest));
+
+        await using var stringWriter = new StringWriter();
+        serializer.Serialize(stringWriter, requestData);
+        using var queryRequest = new HttpRequestMessage(new HttpMethod("QUERY"),"/search");
+        queryRequest.Content = new StringContent(stringWriter.ToString(), Encoding.UTF8, MediaTypeNames.Application.Xml);
+
+        // Act
+        HttpResponseMessage response = await client.SendAsync(queryRequest, TestContext.Current.CancellationToken);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.UnsupportedMediaType);
+    }
 }
+
+// Duplicating instead of referencing the ones in the Project in order to test the "contract" not the existing implementation
+#pragma warning disable CA1812
+public sealed record SearchRequest(int ProductNumber)
+{
+    public SearchRequest() : this(-1)
+    {
+    }
+}
+internal sealed record SearchResponse(string Sku);
+
+#pragma warning restore CA1812
